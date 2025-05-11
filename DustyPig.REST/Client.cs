25ba89935ef8e9d770003cc67a4a957e18c24f85
Jason Davis <jasondavis303@gmail.com>
2025-05-11 13:36:53 -0600

    Add accessor for HttpClient

--- DustyPig.REST/Client.cs
+++ DustyPig.REST/Client.cs
@@ -68,7 +68,10 @@ public class Client : IDisposable
     }
 
 
-
+    /// <summary>
+    /// The <see cref="HttpClient"/> used by this class
+    /// </summary>
+    public HttpClient HttpClient => _httpClient;
 
 
 
